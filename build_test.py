9d44ee5cf331489cc9afa6139e0f4cb601332ebc
Anand Mitra <mitra@primaryio.com>
2021-05-10 19:43:27 +0530

    option to load json repo/asset list

--- build_test.py
+++ build_test.py
@@ -8,6 +8,41 @@
 from github import Github
 import sys
 import argparse
+import json
+import pprint
+
+class gitTools:
+    "Misc tools to manipulate github"
+    def __init__(self, token):
+        self.token = token
+        self.github = Github(token)
+        self.repo_list = self.github.get_user().get_repos()
+
+    def get_repo(self, name):
+        for repo in self.repo_list:
+            if repo.name == name:
+                return repo
+        return None
+
+    def get_latest_release(self, repo):
+        rel_list = repo.get_releases()
+        latest = rel_list[0]
+        for rel in rel_list:
+            if rel.created_at > latest.created_at:
+                latest = rel
+        return latest
+    
+    def get_asset_url(self, release, asset_name):
+        for asset in release.get_assets():
+            if asset.name == asset_name:
+                return asset.url
+        assert False , "No such asset"
+
+    def get_latest_rel_url(self, repo_name, asset_name):
+        repo = self.get_repo(repo_name)
+        rel = self.get_latest_release(repo)
+        return self.get_asset_url(rel, asset_name)
+
 
 parser = argparse.ArgumentParser(description="""Helper utility to manage git release and binaries associated with a release.
 The output is intended for use directly into the jenkins script. Hence the output is short so that
@@ -16,29 +51,24 @@ we can script it.""")
 parser.add_argument('-a', required = True, dest = 'auth', help='Auth token for github access. The steps are documented here https://bit.ly/3xOGgV1')
 parser.add_argument('-r', required = True, dest = 'repo', help='Git repository on when we need to operate.')
 parser.add_argument('-f', required = True, dest = 'file',help='Name of asset/file which we want to operate on.')
-parser.add_argument('operation', choices = ["download", "upload"], help='Operation to perfrom download/upload asset file.')
+parser.add_argument('operation', choices = ["download", "upload", "json"], help='Operation to perfrom download/upload asset file. In json we provide a json file with list of repositories and files')
 args = parser.parse_args()
 
 token=args.auth
 get_repo=args.repo
-file_asset = args.file
 operation = args.operation
+file_asset = args.file
 
-g = Github(token)
-for repo in g.get_user().get_repos():
-   if repo.name == get_repo:
-        break;
-else:
-        repo = None
-
-rel_list = repo.get_releases()
-latest = rel_list[0]
-for rel in rel_list:
-    if rel.created_at > latest.created_at:
-        latest = rel
+g = gitTools(token)
 
 if operation.lower() == "download":
-    for asset in latest.get_assets():
-        if asset.name == file_asset:
-            print(asset.url)
-    
+    url = g.get_latest_rel_url(get_repo, file_asset)
+    print(url)
+
+if operation.lower() == "json":
+    with open(file_asset,'r',encoding = 'utf-8') as f:
+        json_data = json.load(f)
+        for repo in json_data:
+            for asset in json_data[repo]:
+                url = g.get_latest_rel_url(repo, asset)
+                print(url)
